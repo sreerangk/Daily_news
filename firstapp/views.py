3aa9f604bd034dd6581cf39616553c2efcf936c7
sreerang k <sreerangk77@gmail.com>
2022-09-14 21:40:30 +0530

    add forms

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from firstapp.forms import UniversityForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {'form': UniversityForm()}
+    return render(request, 'index.html', context)
     
 
 def base(request):
--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -129,3 +129,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
