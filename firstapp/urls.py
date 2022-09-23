8b6f0269fa67e345efedbbdf69bf2cf65cfa70f4
sreerang k <sreerangk77@gmail.com>
2022-09-23 16:19:29 +0530

    delete searchuser

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('unblock/<int:id>',views.unblock,name='unblock'),
     path('adduser',views.adduser,name='adduser'),
     # path('search', views.search, name='search'),
-    path('search_user', views.search_user, name='search_user'),
+
 
 ]
